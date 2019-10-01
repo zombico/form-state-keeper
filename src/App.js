004152a8134f370dca30f03f07cf54f4c14b7607
zombico <franz@ombi.co>
2019-09-30 22:56:04 -0400

    2 form states and deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
-// import ShippingLabelMaker from './features/shipping-label-maker/shippingLabelMaker'
+import ShippingLabelMaker from './features/shipping-label-maker/shippingLabelMaker'
 import FleetTrainingManual from './features/fleet-training-manual/fleetTrainingManual'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        {/* <ShippingLabelMaker /> */}
-        <FleetTrainingManual />
+      <Router>
+        <Route
+          path="/training"
+          render={() => {                
+            return (
+              <FleetTrainingManual />
+            )
+          }}               
+        />
+        <Route
+          path="/shipping"
+          render={() => {                
+            return (
+              <ShippingLabelMaker />
+            )
+          }}               
+        />
+        </Router>          
       </div>
     );
   }
